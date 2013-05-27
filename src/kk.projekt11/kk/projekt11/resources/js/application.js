b9856cae36bf24b3b7e99861b8ffb5dba5401ba6
Serge Davidov <sd@kreativkombinat.de>
2013-05-27 19:10:50 +0200

    update

--- src/kk.projekt11/kk/projekt11/resources/js/application.js
+++ src/kk.projekt11/kk/projekt11/resources/js/application.js
@@ -1,5 +1,5 @@
 /*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true */
-/*global jQuery:false, document:false, window:false, location:false */
+/*global jQuery:false, document:false, location:false */
 
 (function ($) {
     $(document).ready(function () {
@@ -9,8 +9,38 @@
             // enhancement.
             return;
         }
-        // Add custom javascript calls here
-        // e.g.
-        // $('#my-gallery').scrollable();
+        var $container = $('#isotope-container');
+        $container.isotope({
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            },
+            layoutMode : 'masonry',
+            resizable: false,
+            animationEngine: 'best-available'
+        });
+        if ($('#isotope-container').length) {
+            $container.isotope({filter: '*'});
+            var selectionLinks = $('a[data-appui="contentfilter"]');
+            var current = '';
+            $('a[data-appui="contentfilter"]').click(function (e) {
+                e.preventDefault();
+                current = $(this);
+                selectionLinks.not(current).removeClass('active');
+                if ($('#clear-filter').length === 0) {
+                    var target = $(this).parents('ul.navTree');
+                    var url = $(location).attr('hash');
+                    var htmlString = '<li class="navTreeItem visualNoMarker" id="clear-filter">' +
+                        '<a data-appui="contentfilter" data-filter=".teppiche" href="' + url +
+                        '">Alles anzeigen</a>' +
+                        '</li>';
+                    target.prepend(htmlString);
+                }
+                $(this).addClass('active');
+                var selector = $(this).data('filter');
+                $container.isotope({filter: selector});
+            });
+        }
     });
 }(jQuery));
